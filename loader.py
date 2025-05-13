ae3f9aa351a820a6ee4b36f35e661200d9927215
gabriel-intel <g@none.com>
2025-05-13 12:48:15 -0700

    add simulator and rtsp for more realistic simulation

--- loader.py
+++ loader.py
@@ -1,39 +1,75 @@
 import threading
 import queue
 import time
+import cv2
 
 def consumer(q):
     while True:
-        print("Get item for vlm processing...")
+        print(f"{threading.get_ident()} - Get item for vlm processing...")
         item = q.get(block=True, timeout=None)
-        print(f"VLM Processing: {item} Started!")
+        print(f"{threading.get_ident()} - VLM Processing Started for chunk: {item}")
         q.task_done()
-        print("sleeping for vlm processing (simulation)...")
+        print(f"{threading.get_ident()} - Sleeping for vlm processing (simulation)...")
         time.sleep(30) # simulate 30 second VLM chunk processing
 
 def rtsp_producer(q):
+    rtsp_url = "rtsp://127.0.0.1:8554/camera_0"
+    cap = cv2.VideoCapture(rtsp_url)
+    if not cap.isOpened():
+        print("Failed to open RTSP stream.")
+        return
+
     print("Starting rtsp loader thread...", threading.get_ident())
-    for i in range(3000):
+    i = 0
+    chunk_size = 15 * 30 # 15fps * 30 for 30second chunk
+    chunkIdx = 0
+    while True:        
         try:
-            q.put_nowait(f"Item {i}")
-            print(f"{threading.get_ident()} - Produced Chunk {i}")
-            time.sleep(0.43) # guess how long it takes to create 30 second chunk?
+            start_time = time.time()
+            ret, frame = cap.read()
+            i = i + 1
+            #print("Frame capture took: ", time.time()-start_time, " ms")
+            #Frame capture took:  0.09931039810180664  ms
+            #Frame capture took:  0.10014081001281738  ms
+            #Frame capture took:  0.10012626647949219  ms
+            #Frame capture took:  0.10005354881286621  ms
+            #Frame capture took:  0.09984278678894043  ms
+
+            if not ret:
+                print("Stream ended or frame read error.")
+                break
+            if (i % chunk_size) != 0:
+                continue
+            chunkIdx = chunkIdx + 1
+
+            time.sleep(0.15) # Add 15ms for write of video to disk
+            q.put_nowait(f"{threading.get_ident()} - Added chunk {chunkIdx} to queue")
+            print(f"{threading.get_ident()} - Produced Chunk {chunkIdx} at frame index {i}")
+            # Yields ~51750ms latency between 30 second chunk generation
         except queue.Full:
             print(f"{threading.get_ident()} - Queue is full! Dropping frame {i}")
-            time.sleep(0.33) # time to get frame from rtsp stream 
 
 if __name__ == "__main__":
-    q = queue.Queue(maxsize=100)
+    q = queue.Queue(maxsize=10)
+    q2 = queue.Queue(maxsize=10)
+    q3 = queue.Queue(maxsize=10)
+    q4 = queue.Queue(maxsize=10)
    
     print("starting vlm processing thread")
     c = threading.Thread(target=consumer, args=(q,), daemon=True)
+    threading.Thread(target=consumer, args=(q2,), daemon=True).start()
+    threading.Thread(target=consumer, args=(q3,), daemon=True).start()
+    threading.Thread(target=consumer, args=(q4,), daemon=True).start()
     c.start()
 
     # camera 1 simulation thread
     threading.Thread(target=rtsp_producer, args=(q,), daemon=True).start()
 
     # camera 2 simulation thread
-    #threading.Thread(target=rtsp_producer, args=(q,), daemon=True).start()
+    threading.Thread(target=rtsp_producer, args=(q2,), daemon=True).start()
+
+    threading.Thread(target=rtsp_producer, args=(q3,), daemon=True).start()
+    threading.Thread(target=rtsp_producer, args=(q4,), daemon=True).start()
 
 
     print("waiting for q processing...")
